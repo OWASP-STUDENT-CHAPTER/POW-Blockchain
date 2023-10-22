5bada5140b82e6fd2ea1e74cba44b6f13296ee78
Kulwinder <laddiaulakh2002@gmail.com>
2023-10-22 23:35:33 +0530

    added respond with json function utility

--- controllers/block-controllers.go
+++ controllers/block-controllers.go
@@ -85,3 +85,15 @@ func HandleWriteBlock(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, r, http.StatusCreated, newBlock)
 
 }
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
